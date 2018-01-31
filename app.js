1a0f4d7038b5daba3e761e83b4c02e4c6325b12e
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-01-31 21:14:04 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/pages/index.html'));
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
