4edd5e16739b732de50598bb942a2a8d3f695636
Luca Spagnolo <luca.spagnolo91@gmail.com>
2018-04-22 13:59:10 +0800

    test commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/pages/index.html'));
+  res.sendFile(path.join(__dirname + "/pages/index.html"));
 })
 
 app.listen(process.env.PORT || 3000)
